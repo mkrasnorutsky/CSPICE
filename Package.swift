dcf8dff99181c03cbc162cf618ae14c32fe310e1
Mikhail Krasnorutskiy <mikhail@Mikhails-Mac-mini.local>
2026-05-29 18:38:43 +0700

    SPM v1.0.4

--- Package.swift
+++ Package.swift
@@ -18,7 +18,7 @@ let package = Package(
         .binaryTarget(
             name: "CSPICE",
             url: "https://github.com/mkrasnorutsky/CSPICE/releases/download/cspice-1.0.4/CSPICE.xcframework.zip",
-            checksum: "625ac10b36a38a2f7657add60badc0853f416fde7c4fc190330ef9c0633b4068"
+            checksum: "60ee31e730b1744f312935abe0782c5508095c72c33c55e1b2df234fd164869e"
         )
     ]
 )
