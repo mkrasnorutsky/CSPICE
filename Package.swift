89b1ee91a16ef96d0f4d0ca62739df18a8c0f300
Mikhail Krasnorutskiy <mikhail@Mikhails-Mac-mini.local>
2026-06-02 14:02:39 +0700

    Version 1.0.8

--- Package.swift
+++ Package.swift
@@ -18,8 +18,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "CSPICE",
-            url: "https://github.com/mkrasnorutsky/CSPICE/releases/download/1.0.6/CSPICE.xcframework.zip",
-            checksum: "125ae59030790b28f3d5d6a524de4b67552147bca0d3b5d2f649e769c7f8f22e"
+            url: "https://github.com/mkrasnorutsky/CSPICE/releases/download/1.0.8/CSPICE.xcframework.zip",
+            checksum: "dba1c6fed61eb862f4134c2e8a35ff7dd0114eba4acbcca79e4fe4ac67b05d1d"
         )
     ]
 )
