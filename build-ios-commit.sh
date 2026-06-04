4b3fe88d5c4e9f385ea9dc92a6f5397488be60e4
Mikhail Krasnorutskiy <mikhail@Mikhails-Mac-mini.local>
2026-06-04 13:51:55 +0700

    ok

--- build-ios-commit.sh
+++ build-ios-commit.sh
@@ -1,5 +1,5 @@
+#!/usr/bin/env bash
 git commit -m "Release $1"
-git tag $1
-If you also want to push both the commit and the tag to the remote:
+git tag "$1"
 git push origin main
-git push origin $1
+git push origin "$1"
